d0df4bf92f6e35440e2c664d37335dead7365082
Esteban Vargas Quirós <79805608+estebanvquiros@users.noreply.github.com>
2025-11-19 19:17:48 -0600

    Add book constructor and function to add new books
    Implement constructor for `Book` and function to create new books and
    insert them into the library array, as well as some sample books.

---
+++ script.js
@@ -0,0 +1,20 @@
+const myLibrary = [];
+
+function Book(title, author, numberOfPages, readStatus) {
+	this.id = crypto.randomUUID();
+	this.title = title,
+	this.author = author,
+	this.pages = numberOfPages,
+	this.readStatus = readStatus
+}
+
+function addBook(title, author, numberOfPages, readStatus) {
+	const book = new Book(title, author, numberOfPages, readStatus);
+	myLibrary.push(book);
+}
+
+// Simulation of books stored previously in memory
+myLibrary.push(new Book("The Broken Sword", "Poul Anderson", 320, "read"));
+myLibrary.push(new Book("The First Law", "Joe Abercrombie", 537, "unread"));
+
+addBook("The Name of the Wind", "Patrick Rothfuss", 704, "read");
